04ac502fc78f6c67aa9edc8de9839386e8c946d8
MyungHoon Kim <roykimmyunghoon@gmail.com>
2022-09-25 23:10:46 +0900

    auth con service entity 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeORMConfig),
     BoardsModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
